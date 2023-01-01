3c7ee48d0ff9642c7948cfb1c52f6041cb46e2fb
jeferson <jefinho368@gmail.com>
2023-01-01 14:29:49 -0300

    Create UserSpringSecurity

--- src/main/java/com/jefersonteste/demoteste/Configs/SecurityConfig.java
+++ src/main/java/com/jefersonteste/demoteste/Configs/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.jefersonteste.demoteste.Configs;
 
 import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
